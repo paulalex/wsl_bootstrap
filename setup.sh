76545cb9aec9cadbfe4e8bcd51c93f5c472b4092
Paul Ockleford <paul.ockleford@edfenergy.com>
2020-04-07 15:01:52 +0100

    Amended setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ python3.8 --version
 # output to .bashrc
 echo "[INFO] Updating .bashrc"
 
-cat >> ~/.bashrc <<EOF
+cat <<EOF > ~/.bashrc
 alias python="python3.8"
 
 # Execute source on all of the dotfiles
@@ -43,7 +43,7 @@ shopt -s cdspell
 # Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
 [ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh
 
-# Enable tab completion for `g` by marking it as an alias for `git`
+# Enable tab completion for 'g' by marking it as an alias for 'git'
 if type _git &> /dev/null; then
   complete -o default -o nospace -F _git g
 fi
