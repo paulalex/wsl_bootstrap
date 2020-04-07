c8d4e2fe14e163ff67ad37e21c59843603ab1749
Paul Ockleford <paul.ockleford@edfenergy.com>
2020-04-07 14:08:15 +0100

    Added -y flag for deadsnakes repo installation

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ bash ./sync.sh
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y software-properties-common ca-certificates apt-transport-https jq tree rename git-extras
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt install -y python3.8
 python3.8 --version
 
