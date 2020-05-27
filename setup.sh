421822a9a3344d64617f4d26e9fdae669e9dc8d1
Paul Ockleford <paul.ockleford@edfenergy.com>
2020-05-27 10:08:34 +0100

    Added installation of pre-commit

--- setup.sh
+++ setup.sh
@@ -19,50 +19,15 @@ sudo apt install -y python3.8 python3-pip
 echo "[INFO] Installing virtual env wrapper"
 python3.8 -m pip install virtualenvwrapper
 
-# output to .bashrc
-echo "[INFO] Sending Snippet to .bashrc"
-
-cat <<'EOF' >> ~/.bashrc
-alias python="python3.8"
-alias editbash="vi ~/.bashrc"
-
-# Execute source on all of the dotfiles
-for file in ~/.{aliases,git_functions,shell_functions,environment}; do
-  [ -r "${file}" ] && [ -f "${file}" ] && source "${file}"
-done
-
-unset file
-
-# Sourcing virtual env wrapper
-source ~/.local/bin/virtualenvwrapper.sh
-
-# Case-insensitive globbing (used in pathname expansion)
-shopt -s nocaseglob
-
-# Append to the Bash history file, rather than overwriting it
-shopt -s histappend
-
-# Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
-[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh
-
-# Enable tab completion for 'g' by marking it as an alias for 'git'
-if type _git &> /dev/null; then
-  complete -o default -o nospace -F _git g
-fi
-
-# Install serverless framework using NPM, I have added a temporary
-# workaround (aka a hack) because it seems we cannot install without
-# using sudo but in doing so the ~/.config directory is then owned by root
-# and so things like serverless will not run
-sudo chown -R $USER:$(id -gn $USER) ~/.config
-EOF
-
 # Install virtualenv wrapper
 echo "[INFO] Installing virtual env wrapper"
 python3.8 -m pip install virtualenvwrapper
 
 echo "[INFO] Installing awsume"
 python3.8 -m pip install awsume
+
+echo "[INFO] Installing pre-commit"
+python3.8 -m pip install pre-commit
 
 # Stop git asking for password every time you interact with remote
 echo "[INFO] Amending git config credential helper"
@@ -101,6 +66,44 @@ echo "[INFO] Syncing dotfiles to ${HOME}"
      --exclude "sync.sh" \
     --exclude "README.md" \
     -avh --no-perms . ~
+
+# output to .bashrc
+echo "[INFO] Sending Snippet to .bashrc"
+cat <<'EOF' >> ~/.bashrc
+alias python="python3.8"
+alias editbash="vi ~/.bashrc"
+
+# Execute source on all of the dotfiles
+for file in ~/.{aliases,git_functions,shell_functions,environment}; do
+  [ -r "${file}" ] && [ -f "${file}" ] && source "${file}"
+done
+
+unset file
+
+# Sourcing virtual env wrapper
+source ~/.local/bin/virtualenvwrapper.sh
+
+# Case-insensitive globbing (used in pathname expansion)
+shopt -s nocaseglob
+
+# Append to the Bash history file, rather than overwriting it
+shopt -s histappend
+
+# Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
+[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh
+
+# Enable tab completion for 'g' by marking it as an alias for 'git'
+if type _git &> /dev/null; then
+  complete -o default -o nospace -F _git g
+fi
+
+# Install serverless framework using NPM, I have added a temporary
+# workaround (aka a hack) because it seems we cannot install without
+# using sudo but in doing so the ~/.config directory is then owned by root
+# and so things like serverless will not run
+sudo chown -R $USER:$(id -gn $USER) ~/.config
+EOF
+
 
 echo "[INFO] Sourcing ${HOME}/.bashrc"
 source ~/.bashrc
